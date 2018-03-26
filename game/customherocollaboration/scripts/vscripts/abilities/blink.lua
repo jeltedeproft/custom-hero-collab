1678e5e605613fe58a61bc24e4f1425b9d6a9bf5
jelte de proft <jeltedeproft_8@hotmail.com>
2018-03-26 18:38:10 +0200

    fixed hero models fixed sound mp3 bug

--- game/customherocollaboration/scripts/vscripts/abilities/blink.lua
+++ game/customherocollaboration/scripts/vscripts/abilities/blink.lua
@@ -2,33 +2,20 @@ function CheckBlink ( event )
   local hero = event.caster
   local point = event.target_points[1]
 
-  local bound1 = Entities:FindByName(nil, "target_bounds1"):GetAbsOrigin()
-  local x1 = bound1.x
-  local y1 = bound1.y
-  local bound2 = Entities:FindByName(nil, "target_bounds2"):GetAbsOrigin()
-  local x2 = bound2.x
-  local y2 = bound2.y
-  local pointx = point.x
-  local pointy = point.y
-  if pointx > x1 and pointx < x2 then
-    if pointy > y1 and pointy < y2 then
-      return
-    end
-  end
+  -- local bound1 = Entities:FindByName(nil, "target_bounds1"):GetAbsOrigin()
+  -- local x1 = bound1.x
+  -- local y1 = bound1.y
+  -- local bound2 = Entities:FindByName(nil, "target_bounds2"):GetAbsOrigin()
+  -- local x2 = bound2.x
+  -- local y2 = bound2.y
+  -- local pointx = point.x
+  -- local pointy = point.y
+  -- if pointx > x1 and pointx < x2 then
+  --   if pointy > y1 and pointy < y2 then
+  --     return
+  --   end
+  -- end
 
-  local bound3 = Entities:FindByName(nil, "target_pit_bounds1"):GetAbsOrigin()
-  local x1 = bound3.x
-  local y1 = bound3.y
-  local bound4 = Entities:FindByName(nil, "target_pit_bounds2"):GetAbsOrigin()
-  local x2 = bound4.x
-  local y2 = bound4.y
-  local pointx = point.x
-  local pointy = point.y
-  if pointx > x1 and pointx < x2 then
-    if pointy > y1 and pointy < y2 then
-      return
-    end
-  end
 
   hero:Interrupt()
 end
--- game/customherocollaboration/scripts/vscripts/abilities/magic_flight.lua
+++ game/customherocollaboration/scripts/vscripts/abilities/magic_flight.lua
@@ -24,35 +24,21 @@ function Unstuck(event)
     return
   end
 
-  local bound1 = Entities:FindByName(nil, "target_bounds1"):GetAbsOrigin()
-  local x1 = bound1.x
-  local y1 = bound1.y
-  local bound2 = Entities:FindByName(nil, "target_bounds2"):GetAbsOrigin()
-  local x2 = bound2.x
-  local y2 = bound2.y
-  local pointx = point.x
-  local pointy = point.y
-  if pointx > x1 and pointx < x2 then
-    if pointy > y1 and pointy < y2 then
-      hero.lastpos = point
-      return
-    end
-  end
+  -- local bound1 = Entities:FindByName(nil, "target_bounds1"):GetAbsOrigin()
+  -- local x1 = bound1.x
+  -- local y1 = bound1.y
+  -- local bound2 = Entities:FindByName(nil, "target_bounds2"):GetAbsOrigin()
+  -- local x2 = bound2.x
+  -- local y2 = bound2.y
+  -- local pointx = point.x
+  -- local pointy = point.y
+  -- if pointx > x1 and pointx < x2 then
+  --   if pointy > y1 and pointy < y2 then
+  --     hero.lastpos = point
+  --     return
+  --   end
+  -- end
 
-  local bound3 = Entities:FindByName(nil, "target_pit_bounds1"):GetAbsOrigin()
-  local x1 = bound3.x
-  local y1 = bound3.y
-  local bound4 = Entities:FindByName(nil, "target_pit_bounds2"):GetAbsOrigin()
-  local x2 = bound4.x
-  local y2 = bound4.y
-  local pointx = point.x
-  local pointy = point.y
-  if pointx > x1 and pointx < x2 then
-    if pointy > y1 and pointy < y2 then
-      hero.lastpos = point
-      return
-    end
-  end
   --FindClearSpaceForUnit(hero, lastpos, false) 
   hero:SetAbsOrigin(lastpos)
 end
